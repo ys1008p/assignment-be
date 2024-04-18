3b738c3e61ac526f912cf974510dea41eafe5408
yspark <ys1008p@gmail.com>
2024-04-19 08:10:59 +0900

    create-userCloth.dto.ts 불필요한 데이터 삭제

--- src/user/dto/create-userCloth.dto.ts
+++ src/user/dto/create-userCloth.dto.ts
@@ -1,37 +1,12 @@
 import {
-  IsBoolean,
-  IsDate,
   IsInt,
   IsNotEmpty,
-  IsOptional,
 } from 'class-validator';
 
 export class CreateUserClothDto {
-  id: bigint;
-
-  @IsDate()
-  @IsOptional()
-  createat?: Date;
-
-  @IsDate()
-  @IsOptional()
-  updateat?: Date;
-
   @IsInt()
   @IsNotEmpty()
   employerid: bigint;
-
-  @IsInt()
-  @IsNotEmpty()
-  characteruid: bigint;
-
-  @IsBoolean()
-  @IsOptional()
-  isequiped?: boolean;
-
-  @IsInt()
-  @IsOptional()
-  isstatus?: number;
 
   @IsInt()
   @IsNotEmpty()
@@ -40,12 +15,4 @@ export class CreateUserClothDto {
   @IsInt()
   @IsNotEmpty()
   channel: number;
-
-  @IsBoolean()
-  @IsOptional()
-  isgenerated?: boolean;
-
-  @IsBoolean()
-  @IsOptional()
-  islock?: boolean;
 }
